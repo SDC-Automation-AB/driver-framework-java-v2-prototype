bd38e61df0b165fdd5e3d8d3cf885ad2a94f24a0
JurisPuchin <juris.puchin@gmail.com>
2017-10-26 16:41:44 -0700

    Cleanup

--- src/main/java/org/iot/dsa/dslink/dframework/DFWDevice.java
+++
@@ -1,9 +0,0 @@
-package org.iot.dsa.dslink.dframework;
-
-import java.util.LinkedList;
-
-public class DFWDevice {
-    DFCarouselObject parent;
-    LinkedList <DFWPoint> children;
-    int refresh;
-}
--- src/main/java/org/iot/dsa/dslink/dframework/DFWPoint.java
+++
@@ -1,6 +0,0 @@
-package org.iot.dsa.dslink.dframework;
-
-public class DFWPoint {
-    DFWDevice parent;
-    int refresh;
-}
