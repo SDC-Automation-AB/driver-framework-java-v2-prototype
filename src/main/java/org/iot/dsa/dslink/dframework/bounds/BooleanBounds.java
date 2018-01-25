10d65083fa05bd64e70d4a7be4bb56327a577eec
JurisPuchin <juris.puchin@gmail.com>
2018-01-25 14:14:21 -0800

    Added boolean bounds object.

---
+++ src/main/java/org/iot/dsa/dslink/dframework/bounds/BooleanBounds.java
@@ -0,0 +1,27 @@
+package org.iot.dsa.dslink.dframework.bounds;
+
+import org.iot.dsa.node.DSBool;
+import org.iot.dsa.node.DSElement;
+
+import java.util.Random;
+
+/**
+ * @author James (Juris) Puchin
+ * Created on 1/25/2018
+ */
+public class BooleanBounds implements ParameterBounds<Boolean> {
+
+    BooleanBounds() {
+
+    }
+
+    @Override
+    public boolean validBounds(DSElement val) {
+        return val.isBoolean();
+    }
+
+    @Override
+    public DSElement generateRandom(Random rand) {
+        return DSBool.valueOf(rand.nextBoolean());
+    }
+}
